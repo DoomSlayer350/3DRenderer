c8885febb80f7edf8f201505c1b51c93c568696f
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-16 18:22:27 +0100

    Fixed the window hint mistakes and added the core profile

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 int main(){
     glfwInit();
     GLFWwindow* Window;
-    glfwWindowHint(GL_MAJOR_VERSION, 3);
-    glfwWindowHint(GL_MINOR_VERSION, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwCreateWindow(600,600,"3D Renderer",NULL,NULL);
     std::cout<<"Program ran successfully"<<std::endl;
     glfwTerminate();
