a6e8302d756f405f8e052182dde2ac0f0bf01ba3
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-16 20:20:49 +0100

    Initialised GLAD and created a viewport.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,23 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* Window;
-    Window = glfwCreateWindow(600,600,"3D Renderer",NULL,NULL);
+    int WindowWidth = 600;
+    int WindowHeight = 600;
+    Window = glfwCreateWindow(WindowWidth,WindowHeight,"3D Renderer",NULL,NULL);
     if (Window == NULL)
     {
         glfwTerminate();
         std::cout<<"Window == NULL"<<std::endl;
         return -1;
     }
+
+    glfwMakeContextCurrent(Window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        glfwTerminate();
+        std::cout<<"Failed to initialise GLAD"<<std::endl;
+        return -1;
+    }
+    glViewport(WindowWidth,WindowHeight,NULL,NULL);
     std::cout<<"Program ran successfully"<<std::endl;
     glfwTerminate();
     return 0;
