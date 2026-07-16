2c6018a2de58c45461dc51db63ae00f8d30d9d39
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-16 18:19:30 +0100

    Stated the version.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include <GLFW/glfw3.h>
 
 int main(){
-    
+    glfwInit();
+    GLFWwindow* Window;
+    glfwWindowHint(GL_MAJOR_VERSION, 3);
+    glfwWindowHint(GL_MINOR_VERSION, 3);
+    glfwCreateWindow(600,600,"3D Renderer",NULL,NULL);
     std::cout<<"Program ran successfully"<<std::endl;
+    glfwTerminate();
     return 0;
 }
