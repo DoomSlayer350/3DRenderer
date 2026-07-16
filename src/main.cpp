f6be36ad5874c952ebcb1cc594490eaa6f65e884
DoomSlayer350 <bashartullah123@gmail.com>
2026-07-16 18:27:48 +0100

    Made window = created window and the program terminates if window == null

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 
 int main(){
     glfwInit();
-    GLFWwindow* Window;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwCreateWindow(600,600,"3D Renderer",NULL,NULL);
+    GLFWwindow* Window;
+    Window = glfwCreateWindow(600,600,"3D Renderer",NULL,NULL);
+    if (Window == NULL)
+    {
+        glfwTerminate();
+        std::cout<<"Window == NULL"<<std::endl;
+        return -1;
+    }
     std::cout<<"Program ran successfully"<<std::endl;
     glfwTerminate();
     return 0;
